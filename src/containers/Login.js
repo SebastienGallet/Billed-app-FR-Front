e79697e0d3c192c21e407b645adb640161df3336
Sebastien Gallet <sgallet.dev@gmail.com>
2023-05-02 17:04:44 +0200

    vf

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -37,26 +37,7 @@ export default class Login {
 
   }
 
-  // handleSubmitAdmin = e => {
-  //   e.preventDefault()
-  //   const user = {
-  //     type: "Admin",
-  //     email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-  //     password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
-  //     status: "connected"
-  //   }
-  //   this.localStorage.setItem("user", JSON.stringify(user))
-  //   this.login(user)
-  //     .catch(
-  //       (err) => this.createUser(user)
-  //     )
-  //     .then(() => {
-  //       this.onNavigate(ROUTES_PATH['Dashboard'])
-  //       this.PREVIOUS_LOCATION = ROUTES_PATH['Dashboard']
-  //       PREVIOUS_LOCATION = this.PREVIOUS_LOCATION
-  //       document.body.style.backgroundColor="#fff"
-  //     })
-  // }
+
 
   // [BUG REPORT - Login] remplacement de "employee" par "admin
   handleSubmitAdmin = e => {
